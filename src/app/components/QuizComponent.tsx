87ff1151dea73cdf53a9853e55226029825702be
Avneesh Paliya <avneeshcse@gmail.com>
2025-03-24 18:12:27 +0530

    changes for redirect on reward completion

--- src/app/components/QuizComponent.tsx
+++ src/app/components/QuizComponent.tsx
@@ -85,6 +85,9 @@ const QuizComponent = () => {
       setResult(isCorrect ? "Correct! 🎉" : "Wrong answer, try again.");
       if (correctCount + (isCorrect ? 1 : 0) >= 3) {
         setRewardEarned(true);
+        // setTimeout(() => {
+        //   window.history.back();
+        // }, 1000);
       }
     } catch (error) {
       console.error('Error checking answer', error);
@@ -97,15 +100,16 @@ const QuizComponent = () => {
   const callAdvertiserWebhook = async () => {
     try {
       const sdk = k2SDK.getInstance();
-      const rewardsRes = await sdk.markActionComplete(advertiserId, idToken);
+      const closeOnComplete = true;
+      const rewardsRes = await sdk.markActionComplete(advertiserId, idToken, closeOnComplete);
       console.log('✅ rewards sent successfully:', rewardsRes);
     } catch (error) {
       console.error('Error calling advertiser webhook', error);
     }finally {
-      setTimeout(() => {
-        setRewardEarned(false)
-        setCorrectCount(0)
-      }, 3000);
+      // setTimeout(() => {
+      //   setRewardEarned(false)
+      //   setCorrectCount(0)
+      // }, 3000);
     }
   };
 
@@ -113,26 +117,31 @@ const QuizComponent = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-100 text-black">
       <div className="bg-white p-8 rounded-lg shadow-lg w-3/4 max-w-4xl">
         <h1 className="text-center font-bold text-2xl mb-4 text-blue-600">Ultimate Quiz Challenge! 🏆</h1>
-        <h2 className="text-center font-bold">Question:</h2>
-        <p className="text-center">{currentQuestion.question}</p>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <input
-            type="text"
-            value={answer}
-            onChange={handleChange}
-            className="w-full border rounded p-2"
-            required
-          />
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
-            disabled={loading}
-          >
-            {loading ? 'Checking...' : 'Submit Answer'}
-          </button>
-        </form>
-        {result && <p className="text-center mt-4 font-bold">{result}</p>}
-        <p className="text-center mt-2">Correct Answers: {correctCount} / {correctAttemptsRequired}</p>
+        {!rewardEarned && 
+          <>
+            <h2 className="text-center font-bold">Question:</h2>
+            <p className="text-center">{currentQuestion.question}</p>
+            <form onSubmit={handleSubmit} className="space-y-4">
+              <input
+                type="text"
+                value={answer}
+                onChange={handleChange}
+                className="w-full border rounded p-2"
+                required
+              />
+              <button
+                type="submit"
+                className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+                disabled={loading}
+              >
+                {loading ? 'Checking...' : 'Submit Answer'}
+              </button>
+            </form>
+            {result && <p className="text-center mt-4 font-bold">{result}</p>}
+            <p className="text-center mt-2">Correct Answers: {correctCount} / {correctAttemptsRequired}</p>  
+          </>
+        }
+        
         {rewardEarned && <p className="text-green-500 text-center font-bold mt-4">🎉 Congratulations! You earned a reward! 🎉</p>}
       </div>
     </div>
